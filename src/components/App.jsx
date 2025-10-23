9c4b14da64c6821a24878c58ff95c5b5d00f1e89
heleledd <157613131+heleledd@users.noreply.github.com>
2025-10-23 19:48:32 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Main from './Main.jsx';
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Main() {
+  const [count, setCount] = useState(0);
+    return (
+      <main className="game-container">
+        
+
+      </main>
+    );
+}
